85b3c25e28c9a032a3213b464b44e7e1e20b064f
Khushi2804 <162557539+Khushi2804@users.noreply.github.com>
2025-05-15 06:43:38 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,4 @@
+// alert(3+3) //we are using node.js, not browser
+
+let name ="khushi"
+let age ="23"
---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "khushi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+// accountEmail="kd@gmail.comn"
+accountPassword="8090"
+accountCity="cpur"
+console.log(accountEmail);
+console.table([accountEmail,accountPassword,accountCity,accountState])
